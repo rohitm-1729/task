58f3ecc8e8cdda396f1c0372611879bc2fd2cae4
rohitm-1729 <rohitmundada74@gmail.com>
2019-03-16 14:44:45 +0530

    Improve readability

--- src/PreProcessImage.cpp
+++ src/PreProcessImage.cpp
@@ -51,5 +51,44 @@ PreProcessImage::~PreProcessImage()
     delete[] LoadBuff;
     delete[] Buff12Bit;
 }
+//Functions of the class SepChannels: 
+
+SepChannels::SepChannels()
+{
+
+}
+SepChannels::SepChannels(PreProcessImage &image)
+{
+    this->_width=image._width;
+    this->_height=image._height;
+
+    this->PixelCount=image.PixelCount;
+    this->Byte_Count=image.Byte_Count;
+
+    Red = new uint16_t[PixelCount/4];
+    Gr1 = new uint16_t[PixelCount/4];
+    Gr2 = new uint16_t[PixelCount/4];
+    Blu = new uint16_t[PixelCount/4];
+
+
+}
+int SepChannels::Seperate(PreProcessImage& image)
+{
+    // to seperate 4 colour channels
+    unsigned long long int rgcount=0,gbcount=0;
+
+    for(unsigned long long int  counter = 0; counter < PixelCount-1; counter += 2){
+        if((counter/_width)%2==0){  // case for RGRGRGRGRG....
+            Red[rgcount]=image.Buff12Bit[counter];
+            Gr1[rgcount]=image.Buff12Bit[counter+1];
+            rgcount++;
+        }else{ //case for GBGBGBGBGB....
+            Gr2[gbcount]=image.Buff12Bit[counter];
+            Blu[gbcount]=image.Buff12Bit[counter+1];
+            gbcount++;
+        }
+    
+    }
+}
 
     
--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,28 @@ using namespace std;
 
 int main()
 {
+    //create an Image object
+    cout<<"Loading RAW12 Image into memory.."<<endl;
     PreProcessImage Image(WIDTH,HEIGHT);
+    //Seperate channels of the Image 
+    cout<<"Seperating R,G,G,B channels"<<endl;
     SepChannels Channels(Image);
     Channels.Seperate(Image);
+
+    cout<<"Debayering the image to a colored one.."<<endl;
     Debayer Debayered(Channels);
     Debayered.NearestNeighbour();
+    
+    cout<<"Converting data to 8 bit format :"<<endl;
     To8Bit EightBitImage(Debayered,Image);
     EightBitImage.ConvertTo8();
+    cout<<"Intensity Values of 5x5 tile:"<<endl;
     EightBitImage.PrintIntensityVals();
+    
+    cout<<"Writing Image to PPM format"<<endl;
     WriteImage WriteFile(EightBitImage);
     WriteFile.WriteBoth();
+    
+    cout<<"Check Images in "<<PGMIMG<<endl;
     return 0;
 }
